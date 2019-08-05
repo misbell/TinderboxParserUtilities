7de8e252b47aeac8ebc3666bb65b5688f57cb595
Michael Prenez-Isbell <michael.isbell@gmail.com>
2019-08-05 19:08:43 -0400

    first commit

---
+++ TinderboxParserUtilities/TinderboxParserUtilities.h
@@ -0,0 +1,13 @@
+//
+//  TinderboxParserUtilities.h
+//  TinderboxParserUtilities
+//
+//  Created by michael isbell on 8/5/19.
+//  Copyright © 2019 michael isbell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TinderboxParserUtilities : NSObject
+
+@end
---
+++ TinderboxParserUtilities/TinderboxParserUtilities.m
@@ -0,0 +1,13 @@
+//
+//  TinderboxParserUtilities.m
+//  TinderboxParserUtilities
+//
+//  Created by michael isbell on 8/5/19.
+//  Copyright © 2019 michael isbell. All rights reserved.
+//
+
+#import "TinderboxParserUtilities.h"
+
+@implementation TinderboxParserUtilities
+
+@end
